5bde05b14bb8c97f5a95ada26d2a16692a01922e
kiran-gondi <kiran.gondi@gmail.com>
2024-10-15 18:33:52 +0530

    Hook - useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import './App.css';
 import UseCallbackParent from './Hooks/useCallbackParent';
 import UseMemo from './Hooks/useMemo';
+import UseRef from './Hooks/useRef';
 
 function App() {
   //UseCallbackParent
   //return <UseCallbackParent></UseCallbackParent>;
 
   //UseMemo
-  return (<UseMemo data={"test123"}></UseMemo>);
+  // return (<UseMemo data={"test123"}></UseMemo>);
+
+  //UseRef
+  return (<UseRef></UseRef>);
 
 
 }
