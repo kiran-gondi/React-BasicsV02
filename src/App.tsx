af86d012e2e99211e926b81c6f6cf07bbba1c21b
kiran-gondi <kiran.gondi@gmail.com>
2024-10-15 18:52:10 +0530

    Hook - useLayoutEffect

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import './App.css';
 import UseCallbackParent from './Hooks/useCallbackParent';
 import UseMemo from './Hooks/useMemo';
 import UseRef from './Hooks/useRef';
+import UseLayoutEffectHook from './Hooks/useLayoutEffect';
+import React from 'react';
 
 function App() {
   //UseCallbackParent
@@ -11,8 +13,10 @@ function App() {
   // return (<UseMemo data={"test123"}></UseMemo>);
 
   //UseRef
-  return (<UseRef></UseRef>);
+  //return (<UseRef></UseRef>);
 
+  //UseLayoutEffect
+  return (<UseLayoutEffectHook></UseLayoutEffectHook>);
 
 }
 
